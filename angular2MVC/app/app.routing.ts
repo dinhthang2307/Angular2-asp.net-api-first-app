bc835c44176cbdfcd741379fa65ac3a35c1dc6c4
thang-PC\thang <dinhthang2307@gmail.com>
2017-11-06 14:29:40 +0700

    finish project

---
+++ angular2MVC/app/app.routing.ts
@@ -0,0 +1,11 @@
+﻿import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home.component';
+import { UserComponent } from './components/user.component';
+const appRoutes: Routes = [
+    { path: 'user', component: UserComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
